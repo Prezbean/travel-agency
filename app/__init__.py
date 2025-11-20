10cb62ac0e4b14f28e9e4e64b62b3bbf2d284c23
41386471 <41386471@newdur.ac.uk>
2025-11-20 15:45:00 +0000

    [travel_agency]

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,8 @@
 from flask import Flask
 from config import Config
 
-app = Flask(__name__)
-app.config.from_object(Config)
+def create_app():
+    app = Flask(__name__, template_folder='templates', static_folder='static')
+    app.config.from_object(Config)
 
 from app import routes
